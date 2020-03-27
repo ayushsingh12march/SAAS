ffaf96b68d7ec6d962092152c5912f328d498f7d
Ayush <ayushsingh12march@gmail.com>
2020-03-28 00:54:32 +0530

    single todos can be accessed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,10 @@
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is #{DateTime.now.to_s(:short)}"
+    render plain: "#{Todo.order(:due_date).map { |todo| todo.pleasant_string }}"
+  end
+
+  def show
+    todo = Todo.find(params[:id])
+    render plain: "#{todo.pleasant_string}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # todos#index refers to index method in todosController class
   get "todos", to: "todos#index"
+  get "todos/:id", to: "todos#show"
 end
