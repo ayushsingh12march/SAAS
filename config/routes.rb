b3903344272bac18299ee19563d4db544f9d8324
Ayush <ayushsingh12march@gmail.com>
2020-04-06 23:15:26 +0530

    post route created for action create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # todos#index refers to index method in todosController class
   get "todos", to: "todos#index"
+  post "todos/create", to: "todos#create"
   get "todos/:id", to: "todos#show"
 end
