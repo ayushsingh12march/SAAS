f257186fd6a209b544a16569ea803d6ba8aa7cab
Ayush <ayushsingh12march@gmail.com>
2020-04-06 23:23:14 +0530

    resources added instead of custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # todos#index refers to index method in todosController class
-  get "todos", to: "todos#index"
-  post "todos/create", to: "todos#create"
-  get "todos/:id", to: "todos#show"
+
+  # get "todos", to: "todos#index"
+  # post "todos/create", to: "todos#create"
+  # get "todos/:id", to: "todos#show"
+
+  resources :todos
 end
