fbf06adfdfe46d4b61e3ed5593da5a3dce8597e4
Ayush <ayushsingh12march@gmail.com>
2020-04-06 23:15:26 +0530

    post route created for action create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,24 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: "#{Todo.order(:due_date).map { |todo| todo.pleasant_string }}"
+    render plain: Todo.order(:due_date).map { |todo| todo.pleasant_string }.join("\n")
   end
 
   def show
     todo = Todo.find(params[:id])
     render plain: "#{todo.pleasant_string}"
+  end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    response_text = "New todo created, id : #{new_todo.id}"
+    render plain: response_text
   end
 end
