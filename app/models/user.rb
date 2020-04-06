0031fa3ea77a8ae23c11c718af1966d9ae0ef21a
Ayush <ayushsingh12march@gmail.com>
2020-04-07 02:04:06 +0530

    FEAT: login added without encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :name, :email, :password, presence: true  #to confirm all params are enterd while creation
+  validates :name, presence: true  #to confirm all params are enterd while creation
+  validates :email, presence: true
+  validates :password, presence: true
 
   def pleasant_string
     "#{id}. Name : #{name}, Email : #{email} "
