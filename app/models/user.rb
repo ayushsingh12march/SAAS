261ea33e232323b1df69c9bac89e54ada6e2c462
Ayush <ayushsingh12march@gmail.com>
2020-04-07 01:20:15 +0530

    validation during table entry is added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, :email, :password, presence: true  #to confirm all params are enterd while creation
+
   def pleasant_string
     "#{id}. Name : #{name}, Email : #{email} "
   end
