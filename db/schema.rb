7dc284c0093e98feda1c1d7c560dc16d02598e82
Ayush <ayushsingh12march@gmail.com>
2020-04-21 22:15:47 +0530

    Updated database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_185519) do
+ActiveRecord::Schema.define(version: 2020_04_21_163703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "student", id: false, force: :cascade do |t|
-    t.integer "regno"
-    t.string "name", limit: 20
-    t.float "cgpa"
-  end
 
   create_table "todos", force: :cascade do |t|
     t.text "todo_text"
