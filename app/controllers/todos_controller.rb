88f3bd7235ab7a5ea32166f0d70f36df47c35184
Ayush <ayushsingh12march@gmail.com>
2020-05-18 22:17:42 +0530

    create api connected with frontend

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,14 +13,14 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
+    due_date = DateTime.parse(params[:due][:date])
     new_todo = Todo.create!(
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
     )
     response_text = "New todo created, id : #{new_todo.id}"
-    render plain: response_text
+    redirect_to todos_path
   end
 
   def update
